7b491232a7ab676a4237daa0d47490c1645a4778
Hércules Pereira <herculespsilva@hotmail.com>
2020-10-01 17:57:59 -0300

    first commit

---
+++ DDL.sql
@@ -0,0 +1,36 @@
+create schema anotacao;
+
+use anotacao;
+
+create user 'user'@'localhost' identified by 'pass123';
+
+GRANT ALL PRIVILEGES ON * . * TO 'victor'@'localhost';
+
+
+
+create table usr_usuario (
+usr_id bigint unsigned not null auto_increment,
+usr_nome varchar(20) not null,
+usr_senha varchar(50) not null,
+primary key(usr_id),
+unique key uni_usuario_nome  (usr_nome)
+);
+
+create table aut_autorizacao (
+aut_id bigint unsigned not null auto_increment,
+aut_nome varchar(20) not null,
+primary key (aut_id),
+unique key uni_aut_nome (aut_nome)
+);
+
+create table uau_usuario_autorizacao (
+usr_id bigint unsigned not null,
+aut_id bigint unsigned not null,
+primary key(usr_id, aut_id),
+foreign key aut_usuario_fk (usr_id) references usr_usuario(usr_id),
+foreign key aut_autorizacao_fk (aut_id) references aut_autorizacao(aut_id)
+);
+
+insert into usr_usuario (usr_nome,usr_senha) values ('victor','boasenha');
+insert into aut_autorizacao(aut_nome) values ('role_admin');
+insert into uau_usuario_autorizacao values (1,1);
