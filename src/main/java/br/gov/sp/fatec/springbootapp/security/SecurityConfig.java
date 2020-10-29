5188e0afadb17b9a3acfe445baab15300cc1b2a6
herculespsilva <herculespsilva@hotmail.com>
2020-10-29 18:15:50 +0000

    Configuração de PasswordEncoder

--- src/main/java/br/gov/sp/fatec/springbootapp/security/SecurityConfig.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/security/SecurityConfig.java
@@ -1,32 +1,38 @@
 package br.gov.sp.fatec.springbootapp.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().httpBasic().and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf().disable().httpBasic().and()
+                // this disables session creation on Spring Security
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
-    /*
-     * @Autowired private UserDetailsService userDetailsService;/*
-     * 
-     * 
-     * @Override public void configure(AuthenticationManagerBuilder auth) throws
-     * Exception { http.csrf().disable().http
-     * auth.userDetailsService(userDetailsService); }
-     * 
-     * @Bean
-     * 
-     * @Override public AuthenticationManager authenticationManagerBean() throws
-     * Exception { return super.authenticationManagerBean(); }
-     */
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/br/gov/sp/fatec/springbootapp/service/SegurancaServiceImpl.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/service/SegurancaServiceImpl.java
@@ -3,9 +3,14 @@ package br.gov.sp.fatec.springbootapp.service;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +20,7 @@ import br.gov.sp.fatec.springbootapp.exception.RegistroNaoEncontradoException;
 import br.gov.sp.fatec.springbootapp.repository.AutorizacaoRepository;
 import br.gov.sp.fatec.springbootapp.repository.UsuarioRepository;
 
-@Service ("segurancaService")
+@Service("segurancaService")
 public class SegurancaServiceImpl implements SegurancaService {
 
     @Autowired
@@ -24,32 +29,22 @@ public class SegurancaServiceImpl implements SegurancaService {
     @Autowired
     private UsuarioRepository usuarioRepo;
 
-    /*serviceImpl-basicamente estamos na camada de negocio, onde estão as operações e regras de negocio.
-    não deve se colocar isso no controller.
-    Controller somente repassa para a camada de negocios, pega a resposta e devolve para a view.
-    */
+    @Autowired
+    private PasswordEncoder passEncoder;
 
-    /*transactional pode ser colocado em cima do metodo ou da classe, se colocado na classe, todos
-    os metodos levarão essa operação*/
-
-    /*Serviços usam varios repositorios, comum.*/
-
-    @Transactional 
-    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Transactional
+    // @PreAuthorize("hasRole('ROLE_ADMIN')")
     public Usuario criarUsuario(String nome, String senha, String autorizacao) {
 
-        Autorizacao aut= autRepo.findByNome(autorizacao);
-        if(aut==null)
-        {
+        Autorizacao aut = autRepo.findByNome(autorizacao);
+        if (aut == null) {
             aut = new Autorizacao();
             aut.setNome(autorizacao);
             autRepo.save(aut);
         }
-
-        
         Usuario usuario = new Usuario();
         usuario.setNome(nome);
-        usuario.setSenha(senha);
+        usuario.setSenha(passEncoder.encode(senha));
         usuario.setAutorizacoes(new HashSet<Autorizacao>());
         usuario.getAutorizacoes().add(aut);
         usuarioRepo.save(usuario);
@@ -59,41 +54,47 @@ public class SegurancaServiceImpl implements SegurancaService {
 
     @Override
     @PreAuthorize("isAuthenticated()")
-    //@PostAuthorize
-    public List<Usuario>buscarTodosUsuarios()
-    {
+    // @PostAuthorize
+    public List<Usuario> buscarTodosUsuarios() {
         return usuarioRepo.findAll();
-    }
-    
-    @Override
-    public Usuario buscarUsuarioPorId(Long id)
-    {
-        Optional<Usuario> usuarioOp= usuarioRepo.findById(id);
-        if(usuarioOp.isPresent())
-        {
-            return usuarioOp.get();
-        }
-         throw new RegistroNaoEncontradoException("usuario nao encontrado!");
     }
 
     @Override
-    public Usuario buscarUsuarioPorNome(String nome)
-    {
+    public Usuario buscarUsuarioPorId(Long id) {
+        Optional<Usuario> usuarioOp = usuarioRepo.findById(id);
+        if (usuarioOp.isPresent()) {
+            return usuarioOp.get();
+        }
+        throw new RegistroNaoEncontradoException("usuario nao encontrado!");
+    }
+
+    @Override
+    public Usuario buscarUsuarioPorNome(String nome) {
         Usuario usuario = usuarioRepo.findByNome(nome);
-        if(usuario!=null)
-        {
+        if (usuario != null) {
             return usuario;
         }
         throw new RegistroNaoEncontradoException("usuario nao encontrado!");
     }
 
-    public Autorizacao buscarAutorizacaoPorNome(String nome)
-    {
-        Autorizacao autorizacao= autRepo.findByNome(nome);
-        if(autorizacao!=null)
-        {
+    public Autorizacao buscarAutorizacaoPorNome(String nome) {
+        Autorizacao autorizacao = autRepo.findByNome(nome);
+        if (autorizacao != null) {
             return autorizacao;
         }
         throw new RegistroNaoEncontradoException("autorizacao nao encontrada!");
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepo.findByNome(username); 
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuário " + username + " não encontrado");
+        }
+        return User.builder().username(username).password(usuario.getSenha())
+            .authorities(usuario.getAutorizacoes().stream()
+                .map(Autorizacao::getNome).collect(Collectors.toList())
+                .toArray(new String[usuario.getAutorizacoes().size()]))
+            .build();
     }
 }
